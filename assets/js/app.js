660ce2b53198ad506cb5d0e54a8ebe55387e978f
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-03 15:55:54 +0530

    OG PUMPIONS section done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,35 @@ const navbarbtn = () => {
     document.getElementById("nav-name").classList.toggle("show-navbar")
     document.body.classList.toggle("overflow-hidden")
 };
+
+
+$('.team-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 800,
+    dots: false,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                dots: true,
+            }
+        }
+    ]
+});
