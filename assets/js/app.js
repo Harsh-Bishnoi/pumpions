30fb8583c89404f631f580703aa3777204fb5431
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-05 09:33:27 +0530

    add back to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,20 @@ $('.team-slider').slick({
     autoplaySpeed: 800,
     dots: true,
 });
+
+
+// back to top
+let backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+backToTopButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
